dc3014931ef99269617043318cb9a36f6382e03b
Talika Bajaj <bajajtalika2001@gmail.com>
2024-05-21 13:15:15 +0530

    Add random color to the note

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const addBtn = document.getElementById('btn');
 const note = document.getElementById('note');
 const tasks = document.getElementById('tasks');
+const colors = ['lightpink', 'bisque', 'burlywood', '#ffcfbf', 'darkseagreen', 'lightblue', 'lightsteelblue', 'mediumaquamarine', 'mistyrose', '#7be6ad', 'thistle'];
+
 
 //get notes from localstorage
 let notes = JSON.parse(localStorage.getItem('notes')) || [];
 //If notes exists in the local storage they will get stored in notes after they are parsed into an array
 //else , notes will be initiakised as an empty array
 
-function enableButton(params) {
+function enableButton() {
   if (note.value == '') {
     addBtn.disabled = true;
     document.getElementById('msg').style.display = 'block';
@@ -15,25 +17,29 @@ function enableButton(params) {
     document.getElementById('msg').style.display = 'none';
     // console.log(note.value);
     addBtn.disabled = false;
-
   }
 }
 
 function renderNotes() {
-  tasks.innerHTML = '';
-  notes.forEach((note, index) => {
-    const displayNote = document.createElement('div');
-    displayNote.className = 'note';
-    displayNote.innerHTML = `
- 
-    <p class="para">${note}</p>
-    <div class="buttons">
+  if (notes.length > 0) {
+    tasks.innerHTML = '';
+    notes.forEach((note, index) => {
+      const displayNote = document.createElement('div');
+      displayNote.className = 'note';
+      displayNote.style.backgroundColor = note.color;
+      displayNote.innerHTML = `
+      <p class="para">${note.note}</p>
+      <div class="buttons">
       <button id='${index}' onclick = 'editNote(this.id)' class="edit">&#9998;</button>
       <button id='${index}' onclick='deleteNote(this.id)' class="del">DEL</button>
-    </div>`;
+      </div>`;
 
-    tasks.appendChild(displayNote);
-  });
+      tasks.appendChild(displayNote);
+
+    });
+  } else {
+    tasks.innerHTML = `<h2 class='empty'>No notes available yet.</h2>`;
+  }
 }
 
 renderNotes();
@@ -44,14 +50,14 @@ function saveNotes() {
 }
 
 
-addBtn.addEventListener('click', ()=> {
+addBtn.addEventListener('click', () => {
   let noteVal = note.value;
   if (noteVal == '') {
-   
     document.getElementById('msg').style.display = 'block';
   } else {
-
-    notes.push(noteVal);
+    let color = colors[Math.floor(Math.random() * colors.length)];
+    console.log(color);
+    notes.push({ note: noteVal, color });
     saveNotes();
     renderNotes();
 
@@ -64,13 +70,13 @@ addBtn.addEventListener('click', ()=> {
 })
 
 
-function deleteNote (index) {
+function deleteNote(index) {
   notes.splice(index, 1);
   saveNotes();
   renderNotes();
 }
 
-function editNote (index) {
+function editNote(index) {
   note.value = notes[index];
   deleteNote(index);
 }
